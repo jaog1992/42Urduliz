3c9aba28f2a8a4c9bed3df5258efe01e266b7e42
Jon Ander Ormaetxea <73747099+jaog1992@users.noreply.github.com>
2022-12-04 22:41:52 +0100

    Add files via upload

---
+++ ft_memcpy.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jde-orma <jde-orma@42urduliz.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/04 21:59:12 by jde-orma          #+#    #+#             */
+/*   Updated: 2022/12/04 21:59:12 by jde-orma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+void *ft_memcpy(void *dest, const void *src, size_t n)
+{
+    unsigned char   *s1;
+    unsigned char   *s2;
+
+    s1 = (unsigned char *)dest;
+    s2 = (unsigned char *)src;
+    if (dest == src)
+            
+    while (n--)
+        *s1++ = *s2++;
+    return (s1);
+}
