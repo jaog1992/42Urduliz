56a04024b661a021902d1a8c9c3009ceea2f3bd9
Jon Ander Ormaetxea <73747099+jaog1992@users.noreply.github.com>
2023-01-03 00:54:14 +0100

    Lift parte 1 terminada (PACO OK)

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: jde-orma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 11:57:33 by jde-orma          #+#    #+#             */
-/*   Updated: 2022/12/14 00:44:52 by jde-orma         ###   ########.fr       */
+/*   Updated: 2022/12/27 19:27:30 by jde-orma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,14 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	i = 0;
 	sc1 = (unsigned char *)s1;
 	sc2 = (unsigned char *)s2;
-	if ((sc1 != sc2) && n != 0)
+	while ((sc1 || sc2) && i < n)
 	{
-		while (i < n)
+		if (sc1[i] != sc2[i])
 		{
-			if (*(sc1 + i) != *(sc2 + i))
-				return ((sc2 + (char)i) - (sc1 + (char)i));
-			i++;
+			return ((sc1[i] + (unsigned char)i) \
+				- (sc2[i] + (unsigned char)i));
 		}
+		i++;
 	}
 	return (0);
 }
--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,27 +6,22 @@
 /*   By: jde-orma <jde-orma@42urduliz.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 08:07:16 by jde-orma          #+#    #+#             */
-/*   Updated: 2022/12/01 08:07:16 by jde-orma         ###   ########.fr       */
+/*   Updated: 2022/12/27 18:21:35 by jde-orma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include  <stdio.h>
 
 int	ft_strncmp(const char *s1, const char *s2, long unsigned int n)
 {
-	unsigned int	i;
+	size_t	i;
 
 	i = 0;
 	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
 	{
-		if (s1[i] > s2[i])
-		{
-			return (1);
-		}
-		else if (s1[i] < s2[i])
-		{
-			return (-1);
-		}
+		if (s1[i] != s2[i])
+			return (((unsigned char) s1[i] - (unsigned char) s2[i]));
 		i++;
 	}
 	return (0);
