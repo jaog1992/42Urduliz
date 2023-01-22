78f1dc8d34adf2c166971b2e763af3f79bbfa91e
Jon Ander Ormaetxea <73747099+jaog1992@users.noreply.github.com>
2023-01-22 04:04:24 +0100

    ft_strtrim bueno

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -3,31 +3,50 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jde-orma <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: jde-orma <jde-orma@42urduliz.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 00:19:55 by jde-orma          #+#    #+#             */
-/*   Updated: 2023/01/03 00:47:20 by jde-orma         ###   ########.fr       */
+/*   Updated: 2023/01/19 18:37:22 by jde-orma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *s1, char const *set)
+int	ft_char_exists(char c, char const *set)
 {
-	unsigned char	*ptr;
-	int				i;
+	size_t	i;
 
 	i = 0;
 	while (set[i])
 	{
-		if (!ft_memchr(s1, set[i], ft_strlen(s1)))
-			return ((char *)s1);
-		else
-		{	
-			ptr = ft_memchr(s1, set[i], ft_strlen(s1));
-			*ptr = '\0';
-		}
+		if (set[i] == c)
+			return (1);
 		i++;
 	}
-	return ((char *)s1);
+	return (0);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*ptr;
+	size_t	i;
+	size_t	start;
+	size_t	end;
+
+	if (!s1)
+		return (NULL);
+	start = 0;
+	while (s1[start] && ft_char_exists(s1[start], set))
+		start++;
+	end = ft_strlen(s1);
+	while (end > start && ft_char_exists(s1[end - 1], set))
+		end--;
+	ptr = (char*)malloc(sizeof(*s1) * (end - start + 1));
+	if (!ptr)
+		return (NULL);
+	i = 0;
+	while (start < end)
+		ptr[i++] = s1[start++];
+	ptr[i] = 0;
+	return (ptr);
 }
