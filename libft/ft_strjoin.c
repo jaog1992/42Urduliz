fa8155f5c044f577bd6188020f12b6be6694c923
jde-orma <jaormaetx@gmail.com>
2023-02-04 23:50:23 +0100

    ft_strjoin corrigiendo calloc por malloc

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: jde-orma <jde-orma@42urduliz.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 08:04:12 by jde-orma          #+#    #+#             */
-/*   Updated: 2023/02/04 21:53:29 by jde-orma         ###   ########.fr       */
+/*   Updated: 2023/02/04 23:46:40 by jde-orma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,10 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	if (!s1 || !s2)
 		return (NULL);
 	len = ft_strlen(s1) + ft_strlen(s2) + 1;
-	ptr = (char *)calloc(len + 1, sizeof(char));
+	ptr = (char *)malloc(sizeof(char) * (len + 1));
 	if (!ptr)
 		return (NULL);
 	ft_strlcpy(ptr, s1, len);
 	ft_strlcat(ptr, s2, len);
-	ptr[len] = '\0';
 	return (ptr);
 }
