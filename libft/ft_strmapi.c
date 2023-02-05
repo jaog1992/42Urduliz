7a03c3e9eaca96afb22558d5ce11f72bb237a83e
Jon Ander Ormaetxea <73747099+jaog1992@users.noreply.github.com>
2023-02-05 01:00:10 +0100

    Update ft_strmapi.c

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -12,6 +12,27 @@
 
 #include "libft.h"
 
+/* ************************************************************************** */
+/*                                                                            */
+/* FUNCTIONALITY                                                              */
+/*                                                                            */
+/* ft_strmapi is a function that creates a new string by applying a provided  */
+/* function (second argument ) to each character of the given string (s).     */
+/*                                                                            */
+/* (The difference with "ft_striteri" is that "ft_strmapi" returns a newly    */
+/* allocated string with the transformed characters, while "ft_striteri"      */
+/* modifies the original string in-place)                                     */
+/*                                                                            */
+/* The specific function provided in the second argument is invoked with two  */
+/* arguments: the index of the character in the string and the character      */
+/* itself.                                                                    */
+/*                                                                            */
+/* EXAMPLE                                                                    */
+/*                                                                            */
+/* ft_strmapi(str, to_upper);                                                 */
+/*                                                                            */
+/* ************************************************************************** */
+
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
 	char	*ptr;
