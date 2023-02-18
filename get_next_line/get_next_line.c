31bc8eb364194bc39304b41a35864471749045c7
Jon Ander Ormaetxea <73747099+jaog1992@users.noreply.github.com>
2023-02-18 16:54:14 +0100

    Main modificado

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -77,28 +77,16 @@ int	main(void)
 	char	*line;
 	int		i;
 	int		fd1;
-	int		fd2;
-	int		fd3;
 
 	fd1 = open("tests/text1.txt", O_RDONLY);
-	fd2 = open("tests/text2.txt", O_RDONLY);
-	fd3 = open("tests/text3.txt", O_RDONLY);
 	i = 1;
 	while (i < 4)
 	{
 		line = get_next_line(fd1);
 		printf("line [%02d]: %s", i, line);
 		free(line);
-		line = get_next_line(fd2);
-		printf("line [%02d]: %s", i, line);
-		free(line);
-		line = get_next_line(fd3);
-		printf("line [%02d]: %s", i, line);
-		free(line);
 		i++;
 	}
 	close(fd1);
-	close(fd2);
-	close(fd3);
 	return (0);
 }
