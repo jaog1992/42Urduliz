dbdc9ba9bfa090852925d52b2558b6e90f4f0872
Jon Ander Ormaetxea <73747099+jaog1992@users.noreply.github.com>
2023-03-05 21:26:37 +0100

    Revisión ft_strjoin

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -50,7 +50,7 @@ char	*ft_strjoin(char *s1, char *s2)
 
 	if (!s1)
 	{
-		s1 = (char *)malloc(1 * sizeof(char));
+		s1 = malloc(sizeof(char));
 		s1[0] = '\0';
 	}
 	if (!s1 || !s2)
@@ -61,9 +61,9 @@ char	*ft_strjoin(char *s1, char *s2)
 	i = -1;
 	j = 0;
 	if (s1)
-		while (s1[++i] != '\0')
+		while (s1[++i])
 			str[i] = s1[i];
-	while (s2[j] != '\0')
+	while (s2[j])
 		str[i++] = s2[j++];
 	str[ft_strlen(s1) + ft_strlen(s2)] = '\0';
 	free(s1);
